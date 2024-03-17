0c5df2b12c76fb5c547dc3ac911186396c5b3c43
Aristóteles Pantaleão <aristotelespantaleao2010@hotmail.com>
2024-03-17 18:04:56 -0300

    Update Logo.jsx

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,10 +1,10 @@
 import './Logo.css'
-import logo from '../../assets/imgs/logo.png'
+import logo from '../../assets/imgs/logo.jpeg'
 import React from 'react'
 
 export default props =>
     <aside className='logo'>
         <a href="/" className="logo">
-            <img src="{logo}" alt="logo" />
+        <img src={require('../../assets/img/logo.jpeg')} alt="logo" />
         </a>
     </aside>
