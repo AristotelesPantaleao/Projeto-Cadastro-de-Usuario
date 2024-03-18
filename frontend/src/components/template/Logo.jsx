1c2b6f3410c2dc616b739b52e8c15a55505704c9
Aristóteles Pantaleão <aristotelespantaleao2010@hotmail.com>
2024-03-18 11:15:26 -0300

    Update Logo.jsx

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,10 +1,11 @@
 import './Logo.css'
 import logo from '../../assets/imgs/logo.webp'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props =>
     <aside className='logo'>
-        <a href="/" className="logo">
+        <Link to="/" className="logo">
         <img src={require('../../assets/imgs/logo.webp')} alt="logo"/>
-        </a>
+        </Link>
     </aside>
