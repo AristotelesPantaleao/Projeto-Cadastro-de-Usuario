7c4f0e85ac5eee69243ea8c812afab1e30174606
Aristóteles Pantaleão <aristotelespantaleao2010@hotmail.com>
2024-03-18 11:15:32 -0300

    Update App.jsx

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -5,10 +5,9 @@ import React from 'react'
 
 import { BrowserRouter } from 'react-router-dom'
 
-import Routes from './Routes'
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Home from '../components/home/Home'
+import Routes from './Routes'
 import Footer from '../components/template/Footer'
 
 export default props =>
